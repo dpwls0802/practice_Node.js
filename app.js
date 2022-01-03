05e46e3ec7a1f35c1703ee5eb1a788828fc48e08
yejin <dpwls0802@naver.com>
2022-01-03 23:59:34 +0900

    static file service

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+app.use(express.static('public')); //정적 파일 제공
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });

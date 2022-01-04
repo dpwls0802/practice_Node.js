019a711a7c6c31be54d7fc845eafc4465d7b6bd0
yejin <dpwls0802@naver.com>
2022-01-04 23:29:26 +0900

    static file service

--- app.js
+++ app.js
@@ -7,6 +7,10 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
+app.get('/route', (req, res) => {
+  res.send('Hello Router, <img src="/background.png">');
+});
+
 app.get('/login', (req, res) => {
   res.send('login please!');
 });

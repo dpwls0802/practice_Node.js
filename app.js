097d494537f975ff7df02d9088ba6c31702ffa21
yejin <dpwls0802@naver.com>
2022-01-05 00:13:42 +0900

    static vs dynamic

--- app.js
+++ app.js
@@ -11,6 +11,30 @@ app.get('/route', (req, res) => {
   res.send('Hello Router, <img src="/background.png">');
 });
 
+app.get('/dynamic', (req, res) => { //동적
+  var lis='';
+  for(var i=0; i<5; i++) {
+    lis = lis + '<li>coding</li>';
+  }
+  var time = Date();
+  var output= `
+  <!DOCTYPE html>
+  <html>
+    <head>
+      <meta charset="utf-8">
+      <title></title>
+    </head>
+    <body>
+      Hello Dynamic!
+      <ul>
+        ${lis}
+      </ul>
+      ${time}
+    </body>
+  </html>`;
+  res.send(output);
+});
+
 app.get('/login', (req, res) => {
   res.send('login please!');
 });
